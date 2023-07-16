34437301c2696708fb6b793ec6a11c2a84bc8d7b
Krimx-xav <rakoze.krimx@gmail.com>
2023-07-16 23:53:49 +0200

    I added the changing of colors options

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let color = "black";
+
 function customizeBoard(size) {
     let board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
@@ -8,8 +10,8 @@ function customizeBoard(size) {
     let total = size * size;
     for (let i = 0; i <= total; i++) {
         let square = document.createElement("div");
-        square.addEventListener('mouseover', () => { square.style.backgroundColor = "black"; });
-        square.style.backgroundColor = "blue";
+        square.addEventListener('mouseover', colorSquare);
+        square.style.backgroundColor = "white";
         board.insertAdjacentElement("beforeend", square);
     }
 }
@@ -19,7 +21,22 @@ function changeSize(input) {
     if (input >= 2 && input <= 100) {
         customizeBoard(input);
     } else {
-        console.log("You chose too much squares");
+        prompt("You exceed the set squares choice");
     }
 
+}
+
+function colorSquare() {
+    if (color === 'random') {
+
+        // got this tip on stackoverflow.com
+        this.style.backgroundColor = `hsla(${Math.random() * 360}, 100%, 50%, 1)`
+    } else {
+        this.style.backgroundColor = color;
+    }
+
+}
+
+function changeColor(choice) {
+    color = choice;
 }
