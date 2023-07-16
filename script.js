0778d79bb6b9efcabdd68fab6c9969954821a299
Krimx-xav <rakoze.krimx@gmail.com>
2023-07-16 23:16:39 +0200

    added a change in script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ function customizeBoard(size) {
     let board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
     squares.forEach((div) => div.remove());
-    board.style.gridTemplateColumns = `repeat(${size}, ifr)`;
-    board.style.gridTemplateRows = `repeat(${size}, ifr)`;
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     let total = size * size;
     for (let i = 0; i <= total; i++) {
         let square = document.createElement("div");
+        square.addEventListener('mouseover', () => { square.style.backgroundColor = "black"; });
         square.style.backgroundColor = "blue";
         board.insertAdjacentElement("beforeend", square);
     }
